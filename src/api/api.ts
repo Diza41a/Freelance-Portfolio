4b69fc16601bc7561159d13fb63a228acb8176a2
Davyd Zakorchennyi <davydzakorchennyi@Davyds-Mac-mini.local>
2025-04-08 15:26:36 -0400

    @fix - PRODUCTION_SERVER_URL typo fix;

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const PRODUCTION_SERVER_URL = 'freelance-portfolio-server-lake.vercel.app';
+const PRODUCTION_SERVER_URL = 'https://freelance-portfolio-server-lake.vercel.app';
 const DEVELOPMENT_SERVER_URL = 'http://localhost:8080';
 const baseURL =
   process.env.NODE_ENV == 'production' ? PRODUCTION_SERVER_URL : DEVELOPMENT_SERVER_URL;
